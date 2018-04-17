1a2fcfee8aa710c5724f64aa4b55a072fc035452
wim glenn <wim.glenn@gmail.com>
2018-04-17 12:18:19 -0500

    tests for safe_dump

--- test_oyaml.py
+++ test_oyaml.py
@@ -1,34 +1,68 @@
 import sys
 from collections import OrderedDict
+from types import GeneratorType
 
 import pytest
+from yaml.representer import RepresenterError
 
 import oyaml as yaml
 
 
-d = OrderedDict([('k1', 'v1'), ('k3', 'v3'), ('k2', 'v2')])
+data = OrderedDict([('x', 1), ('z', 3), ('y', 2)])
 
 
 def test_dump():
-    assert yaml.dump(d) == '{k1: v1, k3: v3, k2: v2}\n'
+    assert yaml.dump(data) == '{x: 1, z: 3, y: 2}\n'
+
+
+def test_safe_dump():
+    assert yaml.safe_dump(data) == '{x: 1, z: 3, y: 2}\n'
+
+
+def test_dump_all():
+    assert yaml.dump_all(documents=[data, {}]) == '{x: 1, z: 3, y: 2}\n--- {}\n'
+
+
+def test_safe_dump_all():
+    assert yaml.safe_dump_all(documents=[data, {}]) == '{x: 1, z: 3, y: 2}\n--- {}\n'
 
 
 def test_load():
-    loaded = yaml.load('{k1: v1, k3: v3, k2: v2}')
-    assert loaded == {'k1': 'v1', 'k3': 'v3', 'k2': 'v2'}
+    loaded = yaml.load('{x: 1, z: 3, y: 2}')
+    assert loaded == {'x': 1, 'z': 3, 'y': 2}
+
+
+def test_load_all():
+    gen = yaml.load_all('{x: 1, z: 3, y: 2}\n--- {}\n')
+    assert isinstance(gen, GeneratorType)
+    ordered_data, empty_dict = gen
+    assert empty_dict == {}
+    assert ordered_data == data
 
 
 @pytest.mark.skipif(sys.version_info >= (3,7), reason="requires python3.6-")
 def test_loads_to_ordered_dict():
-    loaded = yaml.load('{k1: v1, k3: v3, k2: v2}')
+    loaded = yaml.load('{x: 1, z: 3, y: 2}')
     assert isinstance(loaded, OrderedDict)
 
 
 @pytest.mark.skipif(sys.version_info < (3,7), reason="requires python3.7+")
 def test_loads_to_std_dict():
-    loaded = yaml.load('{k1: v1, k3: v3, k2: v2}')
+    loaded = yaml.load('{x: 1, z: 3, y: 2}')
     assert not isinstance(loaded, OrderedDict)
     assert isinstance(loaded, dict)
+
+
+def test_subclass_dump():
+
+    class MyOrderedDict(OrderedDict):
+        pass
+
+    data = MyOrderedDict([('x', 1), ('y', 2)])
+    assert '!!python/object/apply:test_oyaml.MyOrderedDict' in yaml.dump(data)
+    with pytest.raises(RepresenterError) as cm:
+        yaml.safe_dump(data)
+    assert str(cm.value) == "cannot represent an object: MyOrderedDict([('x', 1), ('y', 2)])"
 
 
 def test_anchors_and_references():
