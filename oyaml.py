b73cef0d74e899d8b5c6de49d1ee593bb4c5074b
wim glenn <wim.glenn@gmail.com>
2018-04-17 12:19:21 -0500

    Opt-in representer for collections.OrderedDict

--- oyaml.py
+++ oyaml.py
@@ -18,6 +18,7 @@ def map_constructor(loader, node):
 
 pyyaml.add_representer(dict, map_representer)
 pyyaml.add_representer(OrderedDict, map_representer)
+pyyaml.add_representer(OrderedDict, map_representer, Dumper=pyyaml.dumper.SafeDumper)
 
 
 if sys.version_info < (3, 7):
